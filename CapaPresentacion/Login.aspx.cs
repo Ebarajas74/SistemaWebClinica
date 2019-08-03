34c4577b9ccc344713e697e7152a31271a96b7f6
Ebarajas74 <enbace74@gmail.com>
2019-08-02 19:16:26 -0500

    Add Login

---
+++ CapaPresentacion/Login.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CapaPresentacion
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (!Page.IsPostBack)
+            //{
+            //    Session["UserSessionId"] = null;
+            //}
+        }
+
+        protected void LoginUser_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            //bool auth = Membership.ValidateUser(LoginUser.UserName, LoginUser.Password);
+
+            //if (auth)
+            //{
+            //    Empleado objEmpleado = EmpleadoLN.getInstance().AccesoSistema(LoginUser.UserName, LoginUser.Password);
+
+            //    if (objEmpleado != null)
+            //    {
+            //        SessionManager _SessionManager = new SessionManager(Session);
+            //        //SessionManager.UserSessionId = objEmpleado.ID.ToString();
+            //        _SessionManager.UserSessionEmpleado = objEmpleado;
+            //        FormsAuthentication.RedirectFromLoginPage(LoginUser.UserName, false);
+            //    }
+            //    else
+            //    {
+            //        Response.Write("<script>alert('USUARIO INCORRECTO.')</script>");
+            //    }
+            //}
+        }
+
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
